dc68a78d8923f3d5ebffb39e7c3ade5249cf4074
javiercoulon <javiercoulon@gmail.com>
2015-11-19 12:05:59 -0600

    fechar troca entre os usuario e mudanza no banco de dados #2

--- resources/views/livros/jaSolicitado.blade.php
+++ resources/views/livros/jaSolicitado.blade.php
@@ -1,1 +1,15 @@
-já solicitado
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container-fluid">
+    <h1>Solicitacao nao concluida<small> - Já existe uma solicitacao previa</small></h1>
+
+    <div>
+        <a class="btn btn-info"
+        href="<?php echo action('LivroController@getFeed',['pag' =>0 ]);?>"> Ver mais livros</a>
+    </div>
+
+</div>
+
+@stop
--- resources/views/livros/solicitado.blade.php
+++ resources/views/livros/solicitado.blade.php
@@ -1,1 +1,15 @@
-solicitado
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container-fluid">
+    <h1>Solicitacao feita<small> - Agora tem que aguardar para que o usuario responda</small></h1>
+
+    <div>
+        <a class="btn btn-info"
+        href="<?php echo action('LivroController@getFeed',['pag' =>0 ]);?>"> Ver mais livros</a>
+    </div>
+
+</div>
+
+@stop
